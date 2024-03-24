58df370b49c06dacb6844d4ed1d8774db8f871e5
moeznejah <moeznejah@gmail.com>
2024-03-24 14:23:10 +0100

    Automated backup 2024-03-24 14:23:10

--- backup.sh
+++ backup.sh
@@ -7,9 +7,6 @@ GITHUB_TOKEN="your_personal_access_token"
 # Navigate to the Home Assistant configuration directory
 cd /config
 
-# Pull changes from the remote repository
-git pull origin master
-
 # Run Home Assistant's backup service
 ha backup create --name "Backup $(date +"%Y-%m-%d")"
 
@@ -19,5 +16,5 @@ git add .
 # Commit changes with a timestamp
 git commit -m "Automated backup $(date +"%Y-%m-%d %H:%M:%S")"
 
-# Push changes to your GitHub repository using personal access token
-git push https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/moez-nejah/homeAssistant.git master
+# Force push changes to your GitHub repository using personal access token
+git push --force https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/moez-nejah/homeAssistant.git master
